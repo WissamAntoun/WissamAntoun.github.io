3be29accdc0f265ec0eeac50ff40350eff5279ff
wissam antoun <wissam.antoun@gmail.com>
2020-09-18 17:44:35 +0300

    test website

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Wissam\'s Blog', // Required
+  author: 'Wissam Antoun', // Required
+  description: 'Graduate Researcher and AI Engineer',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    website: 'https://WissamAntoun.github.io',
+    github: 'https://github.com/WissamAntoun',
+    twitter: 'https://twitter.com/wissamantoun',
+    linkedin: 'https://ca.linkedin.com/in/wissamantoun',
+  },
+  pathPrefix: '/devblog',
+  siteUrl: 'https://WissamAntoun.github.io/WissamAntoun.github.io/',
+};
